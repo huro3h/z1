121650a510125a5c2ddd2c16df942dad36472820
huro3h_satoshi <iges11@gmail.com>
2016-02-20 22:11:56 +0800

    チェックボックス訂正

--- chbox.php
+++ chbox.php
@@ -2,7 +2,7 @@
 
 foreach($_POST['reserve'] as $reserve) {
 	echo (htmlspecialchars($reserve, ENT_QUOTES, 'UTF-8'). '<br />');
-	echo '上記の日程でご予約を承りました。複数ご選択頂いた場合は後ほどご連絡差し上げます。';
+	
 }
-
+	echo '上記の日程でご予約を承りました。複数ご選択頂いた場合は後ほどご連絡差し上げます。';
  ?>
