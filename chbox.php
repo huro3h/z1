420d71f37b4d7465ddd52c2c9646aee7de8dfb0a
huro3h_satoshi <iges11@gmail.com>
2016-02-21 09:18:14 +0800

    年齢入力欄追加 数字以外の文字を入力するとエラー並びに全角数字を自動で半角に。

--- chbox.php
+++ chbox.php
@@ -5,4 +5,15 @@ foreach($_POST['reserve'] as $reserve) {
 	
 }
 	echo '上記の日程でご予約を承りました。複数ご選択頂いた場合は後ほどご連絡差し上げます。';
+	echo '<br />';
+	echo '<br />';
+$age = mb_convert_kana($_POST['age'], 'n', 'UTF-8');
+
+if (is_numeric($age)) {
+	echo ($age . '歳');
+} else {
+	echo ('年齢は数字でご記入ください☆');
+
+}
+
  ?>
