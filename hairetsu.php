560d1936549654fd22e335c025f2eb2ed02383a8
huro3h_satoshi <huro3h@gmail.com>
2016-04-13 12:46:10 +0800

    配列の復習

--- hairetsu.php
+++ hairetsu.php
@@ -9,4 +9,31 @@ $a3 = "test3";
 $test = array("$a1","$a2","$a3");
 print_r($test);
 // 配列の中に変数を代入し、それを表示させることも可能
+
+// 標準入力から５行分の入力を配列に代入する
+for($i=0; $i < 5; $i++){
+    $team[] = trim(fgets(STDIN));
+}
+// ここから下に記述
+// $team の3つ目の要素を出力してください
+echo $team[2];
+
+// 標準入力から3行分の入力を配列に代入する
+for($i=0; $i < 3; $i++){
+    $team[] = trim(fgets(STDIN));
+}
+// ここから下に記述
+// $team に「狩人」という要素追加
+$team[] = "狩人";
+print_r($team);
+
+// 標準入力から3行分の入力を配列に代入する
+for($i=0; $i < 3; $i++){
+    $team[] = trim(fgets(STDIN));
+}
+// ここから下に記述　
+// $team の冒頭の要素を削除
+unset($team[0]);
+print_r($team);
+
  ?>
