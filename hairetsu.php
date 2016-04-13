6e4cf2c5a1c11ebba3b018b1580fc3dedf108f67
huro3h_satoshi <huro3h@gmail.com>
2016-04-13 08:02:57 +0800

    配列の中に変数を代入し、それを表示

--- hairetsu.php
+++ hairetsu.php
@@ -2,4 +2,11 @@
 $team = array("a","b","c","d");
 // print_r ()内の配列を全て表示する関数
 print_r($team);
+
+$a1 = "test1";
+$a2 = "test2";
+$a3 = "test3";
+$test = array("$a1","$a2","$a3");
+print_r($test);
+// 配列の中に変数を代入し、それを表示させることも可能
  ?>
